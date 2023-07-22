e5b95eaa563f6a4f29d9d2e68a3ba118a9c84510
Brahm V <brahmvanh@gmail.com>
2023-07-22 18:49:33 -0400

    Created some json data for suspension

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -3,25 +3,40 @@ const { gql } = require('apollo-server-express');
 const typeDefs = gql`
 
 	type suspension {
+		_id: ID!
 		suspensionComponentType: String
+		year: String
 		manufacturer: String
 		modelName: String
 		modelNumber: String
-		oilType: String
-		alternatives: String
-		oilVolume: String
+		damper: String
+		spring: String
+		oilVolumes: [oilVolumes]
 		serviceKitNumbers: String
 		serviceInterval: String
 	}
 
 	type suspensionFluid {
+		_id: ID!
 		manufacturer: String
 		modelName: String
 		modelNumber: String
 		weight: String
 	}
 
-	
+	type oilVolumes {
+		damperSide: Boolean
+		springSide: Boolean
+		upperTube: Boolean
+		lowerLeg: Boolean
+		volume: String
+		oilType: [suspensionFluid]
+		weight: String
+
+
+	}
+
+
 	type Query {}
 	type Mutation {}
 `;
