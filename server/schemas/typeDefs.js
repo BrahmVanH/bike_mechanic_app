584d19a62819dfb18f75dab3f1a131ba52267e68
Brahm V <brahmvanh@gmail.com>
2023-07-22 18:01:57 -0400

    created types and notes (ignored)

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,6 +1,27 @@
 const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
+
+	type suspension {
+		suspensionComponentType: String
+		manufacturer: String
+		modelName: String
+		modelNumber: String
+		oilType: String
+		alternatives: String
+		oilVolume: String
+		serviceKitNumbers: String
+		serviceInterval: String
+	}
+
+	type suspensionFluid {
+		manufacturer: String
+		modelName: String
+		modelNumber: String
+		weight: String
+	}
+
+	
 	type Query {}
 	type Mutation {}
 `;
