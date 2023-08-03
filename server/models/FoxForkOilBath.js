ab1dba7085da3c6a40750615be7a5083b69a1da0
Brahm V <brahmvanh@gmail.com>
2023-08-03 17:00:04 -0400

    Created Fox fork oil... model

---
+++ server/models/FoxForkOilBath.js
@@ -0,0 +1,55 @@
+const { Schema, model } = require("mongoose");
+
+const foxForkOilBathSchema = new Schema(
+  {
+    year: {
+      type: String,
+    },
+    model: {
+      type: String,
+      required: true,
+    },
+    damperType: {
+      type: String,
+    },
+    springType: {
+      type: String,
+    },
+    wheelSize: {
+      type: String,
+    },
+    damperUpperVolume: {
+      type: String,
+    },
+    damperUpperOilWt: {
+      type: String,
+    },
+    damperLowerVolume: {
+      type: String,
+    },
+    damperLowerOilWt: {
+      type: String,
+    },
+    springUpperVolume: {
+      type: String,
+    },
+    springUpperOilWt: {
+      type: String,
+    },
+    springLowerVolume: {
+      type: String,
+    },
+    springLowerWt: {
+      type: String,
+    },
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+  }
+);
+
+const FoxForkOilBath = model("FoxForkOilBath", foxForkOilBathSchema);
+
+module.exports = FoxForkOilBath;
