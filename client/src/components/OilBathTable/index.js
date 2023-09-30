18b16a0b14489504e0879e3fcbaa29612db5e685
Brahm V <brahmvanh@gmail.com>
2023-09-30 16:16:59 -0400

    Changed oil back vol table configuration.

--- client/src/components/OilBathTable/index.js
+++ client/src/components/OilBathTable/index.js
@@ -3,40 +3,30 @@ import Handsontable from 'handsontable';
 import 'handsontable/dist/handsontable.full.min.css';
 import './style.css';
 
-
 const OilBathTable = (selectedSuspensionFork) => {
 	const containerRef = useRef(null);
 	useEffect(() => {
 		console.log(selectedSuspensionFork);
-
 	}, [selectedSuspensionFork]);
 
-	
+	const { damperUpperVolume, damperUpperOilWt, damperLowerVolume, damperLowerOilWt, springUpperVolume, springUpperOilWt, springLowerVolume, springLowerOilWt } =
+		selectedSuspensionFork?.selectedSuspensionFork;
 
 	useEffect(() => {
 		if (selectedSuspensionFork) {
 			const data = [
-				[
-					selectedSuspensionFork?.selectedSuspensionFork.damperUpperVolume,
-					selectedSuspensionFork?.selectedSuspensionFork.damperUpperOilWt,
-					selectedSuspensionFork?.selectedSuspensionFork.damperLowerVolume,
-					selectedSuspensionFork?.selectedSuspensionFork.damperLowerOilWt,
-					selectedSuspensionFork?.selectedSuspensionFork.springUpperVolume,
-					selectedSuspensionFork?.selectedSuspensionFork.springUpperOilWt,
-					selectedSuspensionFork?.selectedSuspensionFork.springLowerVolume,
-					selectedSuspensionFork?.selectedSuspensionFork.springLowerOilWt,
-				],
+				['Damper', damperUpperVolume, damperUpperOilWt, damperLowerVolume, damperLowerOilWt],
+				['Spring', springUpperVolume, springUpperOilWt, springLowerVolume, springLowerOilWt],
 			];
 
 			const nestedHeaders = [
-				[{ label: 'Oil Bath Volume', colspan: 8 }],
 				[
-					{ label: 'Damper Side', colspan: 4 },
-					{ label: 'Spring Side', colspan: 4 },
+					{ label: '', colspan: 1 },
+					{ label: 'Oil Bath Volume', colspan: 4 },
 				],
+
 				[
-					{ label: 'Upper Tube', colspan: 2 },
-					{ label: 'Lower Tube', colspan: 2 },
+					{ label: '', colspan: 1 },
 					{ label: 'Upper Tube', colspan: 2 },
 					{ label: 'Lower Tube', colspan: 2 },
 				],
@@ -48,14 +38,38 @@ const OilBathTable = (selectedSuspensionFork) => {
 				virtual: true,
 				width: 300,
 				height: 300,
-				columns: [{ width: 25 }, { width: 25 }, { width: 25 }, { width: 25 }, { width: 25 }, { width: 25 }, { width: 25 }, { width: 25 }],
+				columns: [
+					{
+						width: 75,
+						renderer: function (instance, td, row, col, prop, value, cellProperties) {
+							Handsontable.renderers.TextRenderer.apply(this, arguments);
+							if (value === 'Damper' || value === 'Spring') {
+								// Apply custom CSS style for Male cells
+								td.style.color = 'black';
+								td.style.fontWeight = '400';
+								td.style.backgroundColor = '#f0f0f0';
+							}
+						},
+					},
+					{ width: 75 },
+					{ width: 75 },
+					{ width: 75 },
+					{ width: 75 },
+				],
 				nestedHeaders: nestedHeaders,
+				afterGetColHeader: (col, TH) => {
+					// Check the column index you want to change the background color for
+					if (col === 0) {
+						TH.style.backgroundColor = 'white';
+						TH.style.color = 'white';
+						TH.style.border = 'none';
+					}
+				},
 				colHeaders: true, // Display column headers
 				// rowHeaders: true, // Display row headers
 				contextMenu: true, // Enable context menu
 				licenseKey: 'non-commercial-and-evaluation', // Replace with your license key or leave empty for non-commercial use
 			});
-
 
 			return () => {
 				// Destroy the Handsontable instance when the component unmounts
@@ -64,7 +78,7 @@ const OilBathTable = (selectedSuspensionFork) => {
 		}
 
 		return () => {
-			console.log("selectedSuspensionFork variable is undefined");
+			console.log('selectedSuspensionFork variable is undefined');
 		};
 	}, [selectedSuspensionFork]);
 
