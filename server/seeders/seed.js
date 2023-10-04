80228246fbf3b22d07657317d2edffd3a194bdb4
Brahm V <brahmvanh@gmail.com>
2023-10-04 11:08:37 -0400

    Changed name to Plush Lab in packages and .html. Moved unused json seed data to another repo for later use. Removed marzocchi fork information from server side code

--- server/seeders/seed.js
+++ server/seeders/seed.js
@@ -1,22 +1,17 @@
 const db = require("../config/connection");
-const { RockshoxForkOilBath, FoxForkOilBath, MarzocchiForkOilBath } = require("../models");
-const rockshoxForkOilBathSeeds = require("./webScraperData/forkOilBathVolumesRockshox.json");
-const foxForkOilBathSeeds = require("./webScraperData/forkOilBathVolumesFox.json");
-const marzocchiForkOilBathSeeds = require("./webScraperData/forkOilBathVolumesMarzocchi.json");
+const { RockshoxForkOilBath, FoxForkOilBath } = require("../models");
+const rockshoxForkOilBathSeeds = require("./forkOilBathVolumesRockshox.json")
+const foxForkOilBathSeeds = require("./forkOilBathVolumesFox.json");
 
 db.once('open', async () => {
   try {
-    // await db; // Wait for MongoDB connection to establish
 
     await RockshoxForkOilBath.deleteMany({});
     await FoxForkOilBath.deleteMany({});
-    await MarzocchiForkOilBath.deleteMany({});
 
-    // await new Promise((resolve) => setTimeout(resolve, 10000));
 
     await RockshoxForkOilBath.create(rockshoxForkOilBathSeeds);
     await FoxForkOilBath.create(foxForkOilBathSeeds);
-    await MarzocchiForkOilBath.create(marzocchiForkOilBathSeeds);
 
     console.log("done seeding");
     process.exit(0);
