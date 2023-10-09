3a28cd827cabcd7c7f6d0254255ba5214d02b25f
Brahm V <brahmvanh@gmail.com>
2023-10-08 20:17:15 -0400

    I've got some passing tests for the helper functions

--- client/src/__tests__/listSupportedModelYears.test.js
+++ client/src/__tests__/listSupportedModelYears.test.js
@@ -34,7 +34,7 @@ describe('listSupportedModelYears', () => {
 
 describe('checkSpringUpperTubeData', () => {
 	// Positive Test
-	it('should take a string representing a volume and an object containing a particular property and output two strings representing units of mesurement.', () => {
+	it("should return the strings, '' and '', in response to an upper volume of 'grease'", () => {
 		const springUpperTubeInfo = {
 			springUpperVolume: 'Grease',
 			selectedSuspensionFork: {
@@ -52,5 +52,107 @@ describe('checkSpringUpperTubeData', () => {
 		const upperSpringUnitsGrease = checkSpringUpperTubeData(springUpperTubeInfo);
 
 		expect(upperSpringUnitsGrease).toEqual(expectedUnits);
+	});
+	// Positive Test
+	it("should return two strings 'wt and 'mL' if the selectedForkObject has a value ...fork", () => {
+		const springUpperTubeInfo = {
+			springUpperVolume: '10',
+			selectedSuspensionFork: {
+				selectedSuspensionFork: {
+					fork: '35 Gold',
+				},
+			},
+		};
+
+		const expectedUnits = {
+			springUpperOilWtUnits: 'wt',
+			springUpperVolumeUnits: 'mL',
+		};
+
+		const upperSpringUnitsRockshox = checkSpringUpperTubeData(springUpperTubeInfo);
+		expect(upperSpringUnitsRockshox).toEqual(expectedUnits);
+	});
+
+	// Positive Test
+	it("should return the strings '' and 'mL' if there is no ...fork property and springUpperVolume has a value other than blank or grease", () => {
+		const springUpperTubeInfo = {
+			springUpperVolume: '10',
+		};
+
+		const expectedUnits = {
+			springUpperOilWtUnits: '',
+			springUpperVolumeUnits: 'mL',
+		};
+
+		const upperSpringUnitsFox = checkSpringUpperTubeData(springUpperTubeInfo);
+		expect(upperSpringUnitsFox).toEqual(expectedUnits);
+	});
+
+	// Exception test
+	it('should throw an error if springUpperVolume is undefined', () => {
+		const springUpperTubeInfo = {
+			springUpperVolume: null,
+			selectedSuspensionFork: {
+				selectedSuspensionFork: {
+					fork: '35 Gold',
+				},
+			},
+		};
+
+		const err = new Error('springUpperVolume has an unexpected value. Please refresh');
+
+		const checkSpringUpperTubeDataCb = () => {
+			checkSpringUpperTubeData(springUpperTubeInfo);
+		};
+
+		expect(checkSpringUpperTubeDataCb).toThrowError(err);
+	});
+});
+
+describe('checkUpperDamperTubeData', () => {
+	// Positive Test
+	it('should return a string of "" if damperUpperVolume is equal to "bleed"', () => {
+		const damperUpperVolume = `Bleed`;
+		const expectedDamperUnit = '';
+
+		const upperDamperUnit = checkDamperUpperTubeData(damperUpperVolume);
+
+		expect(upperDamperUnit).toEqual(expectedDamperUnit);
+	});
+	// Positive Test
+	it('should return a string of "" if damperUpperVolume is equal to ""', () => {
+		const damperUpperVolume = ``;
+		const expectedDamperUnit = '';
+
+		const upperDamperUnit = checkDamperUpperTubeData(damperUpperVolume);
+
+		expect(upperDamperUnit).toEqual(expectedDamperUnit);
+	});
+	// Positive Test
+	it('should return a string of "" if damperUpperVolume "\n**\n"', () => {
+		const damperUpperVolume = `\n**\n`;
+		const expectedDamperUnit = '';
+
+		const upperDamperUnit = checkDamperUpperTubeData(damperUpperVolume);
+
+		expect(upperDamperUnit).toEqual(expectedDamperUnit);
+	});
+	it('should return a string of "mL" if damperUpperVolume is not null and does not equal "bleed", `\n**\n`, or an empty string ', () => {
+		const damperUpperVolume = `10`;
+		const expectedDamperUnit = 'mL';
+
+		const upperDamperUnit = checkDamperUpperTubeData(damperUpperVolume);
+
+		expect(upperDamperUnit).toEqual(expectedDamperUnit);
+	});
+	
+	// Exception Test
+	it('Should throw an error if damperUpperVolume is null', () => {
+		const damperUpperVolume = null;
+		const expectedErrorMessage = new Error('damperUpperVolume has an unexpected value');
+		const checkDamperUpperTubeDataCb = () => {
+			checkDamperUpperTubeData(damperUpperVolume);
+		};
+		expect(checkDamperUpperTubeDataCb).toThrowError(expectedErrorMessage);
 	});
 });
--- client/src/utils/helpers.js
+++ client/src/utils/helpers.js
@@ -1,10 +1,10 @@
 //Helper Functions
 
 export const listSupportedModelYears = (yearRange) => {
-  if (typeof yearRange.latestYear !== 'number' || typeof yearRange.oldestYearMinusOne !== 'number' ) {
-    throw new Error("Expected parameter 'yearRange' to contain two numbers")
-  }
-  const {latestYear, oldestYearMinusOne} = yearRange;
+	if (typeof yearRange.latestYear !== 'number' || typeof yearRange.oldestYearMinusOne !== 'number') {
+		throw new Error("Expected parameter 'yearRange' to contain two numbers");
+	}
+	const { latestYear, oldestYearMinusOne } = yearRange;
 	let yearsSupported = [];
 	for (let i = latestYear; i > oldestYearMinusOne; i--) {
 		yearsSupported.push(`${i}`);
@@ -15,6 +15,9 @@ export const listSupportedModelYears = (yearRange) => {
 export const checkSpringUpperTubeData = (springUpperTubeInfo) => {
 	const { springUpperVolume, selectedSuspensionFork } = springUpperTubeInfo;
 
+	if (springUpperVolume === null) {
+		throw new Error('springUpperVolume has an unexpected value. Please refresh');
+	}
 	let springUpperVolumeUnits;
 	let springUpperOilWtUnits;
 	// Change the units for volume and presence of 'wt' based on type of lubricant in spring
@@ -30,18 +33,18 @@ export const checkSpringUpperTubeData = (springUpperTubeInfo) => {
 	}
 
 	return { springUpperVolumeUnits, springUpperOilWtUnits };
-	
-}
+};
 
 export const checkDamperUpperTubeData = (damperUpperVolume) => {
 	let damperUpperVolumeUnits;
-	if (damperUpperVolume === `\n**\n` || damperUpperVolume.toLowerCase() === "bleed") {
+	if (damperUpperVolume === null) {
+		throw new Error('damperUpperVolume has an unexpected value');
+	}
+	if (damperUpperVolume === `\n**\n` || damperUpperVolume.toLowerCase() === 'bleed' || damperUpperVolume === '') {
 		damperUpperVolumeUnits = '';
 	} else {
 		damperUpperVolumeUnits = 'mL';
 	}
 
 	return damperUpperVolumeUnits;
-}
-
-
+};
